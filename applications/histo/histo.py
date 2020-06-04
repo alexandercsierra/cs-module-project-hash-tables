acbb092f7bafdfed7e75b4851f220e3c0be4fe1f
Alexander Sierra <alexandercsierra@gmail.com>
2020-06-04 01:21:31 -0400

    completed histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,22 +1,38 @@
 import re
 
-def word_count(text):
+
+with open('robin.txt') as f:
+    words = f.read()
+
+
+
+def histo(text):
     word_count = {}
     word_list = text.split()
+    longest_word = ''
     for word in word_list:
         word = re.sub(r'[^\w\']', '', word)
         word = word.lower()
+        if len(word) > len(longest_word):
+            longest_word = word
         if word in word_count:
             word_count[word] += "#"
         else:
             if word != '':
                 word_count[word] = "#"
     items = list(word_count.items())
-    items.sort(key=lambda e: len(e[1]))
+    items.sort(key=lambda e: (-len(e[1]), e))
     word_count = dict(items)
+    spaces = [' '] * (len(longest_word) +1)
+    spaces_str = " "
+    spaces_str.join(spaces)
     for word in word_count:
-        print(f'{word} {word_count[word]}\n')
+        if word is not longest_word:
+            print(f'{word} {spaces_str.join(spaces)[:len(longest_word)-len(word)]}{word_count[word]}')
+        else:
+            print(f'{word} {word_count[word]}')
 
 
 
-word_count('hello hello ha ha ha hello ha')
+
+histo(words)
